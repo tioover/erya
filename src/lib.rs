78761d9a2e64a238f9fd09165f9d9ce9803e1960
tioover <tioover@gmail.com>
2015-11-18 17:50:46 +0800

    Add simple example

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+extern crate nalgebra;
+extern crate glium;
+
+
+#[macro_export]
+macro_rules! na {
+    ($x: expr) =>
+        ($crate::na::Vec1::new($x));
+    ($x: expr, $y: expr) =>
+        ($crate::na::Vec2::new($x, $y));
+    ($x: expr, $y: expr, $z: expr) =>
+        ($crate::na::Vec3::new($x, $y, $z));
+    ($x: expr, $y: expr, $z: expr, $w: expr) =>
+        ($crate::na::Vec4::new($x, $y, $z, $w));
+}
+
+
+pub use nalgebra as na;
+pub use glium::Display;
+
+use std::string::ToString;
+use glium::glutin::WindowBuilder;
+use glium::DisplayBuild;
+
+
+pub fn build_display<T: ToString>(title: T, (width, height): (u32, u32)) -> Display {
+    WindowBuilder::new()
+        .with_title(title.to_string())
+        .with_dimensions(width, height)
+        .with_vsync()
+        .build_glium()
+        .unwrap()
+}
